d6e7e874378a820c5046f805ccc603510295b79f
Jese H <pilers2000@hotmail.com>
2023-07-19 09:56:36 -0400

    Added all but one quiz

--- Quizzes/quizScript.js
+++ Quizzes/quizScript.js
@@ -2,6 +2,10 @@ var homeEl = document.querySelector("#homeId");
 var mainEl = document.querySelector("#mainId");
 var scoreEl = document.querySelector("#scoreId");
 var congrats = document.querySelector("#congrats");
+var subjectEl = document.querySelector("#subjectId");
+subjectEl.style.fontSize = "1px";
+subjectEl.style.color = "white";
+var subject = subjectEl.textContent;
 
 var correctEl1 = document.querySelector("#correct1");
 var correctEl2 = document.querySelector("#correct2");
@@ -16,6 +20,40 @@ var answersEl4 = document.querySelector("#answers4");
 var answersEl5 = document.querySelector("#answers5");
 
 var points = 0;
+
+const apiKey = 'AIzaSyA6K492K_D_9u5sxROWzeX2eZcz7AsYZ8Y';
+
+var randomNum = Math.floor(Math.random() * 20);
+console.log(randomNum);
+
+// Function to fetch a video about calculus and embed it into the website
+function fetchAndEmbedVideo() {
+  const apiUrl = 'https://youtube.googleapis.com/youtube/v3/search';
+  const params = {
+    key: apiKey,
+    part: 'snippet',
+    q: subject,
+    type: 'video',
+    maxResults: 100,
+  };
+
+  const url = new URL(apiUrl);
+  Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.items.length);
+      const videoId = data.items[randomNum].id.videoId;
+      console.log(data);
+      const videoUrl = `https://www.youtube.com/embed/${videoId}`;
+      const embedHtml = `<iframe width="560" height="315" src="${videoUrl}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`;
+      document.getElementById('videoContainer').innerHTML = embedHtml;
+    })
+    .catch(error => {
+      console.error('Error fetching video:', error);
+    });
+}
 
 answersEl1.addEventListener("click", function(event) {
     var element = event.target;
@@ -94,11 +132,11 @@ answersEl1.addEventListener("click", function(event) {
       scoreEl.textContent = points + "/5";
       scoreEl.style.fontSize = "30px";
       scoreEl.style.textAlign = "center";
+
+    // Call the function to fetch and embed the calculus video
+    fetchAndEmbedVideo();
+    fetchAndEmbedVideo();
     }
     console.log(points);
   });
-
-homeEl.addEventListener("click", function(){
-    window.open("./Home-Page/home-page.html", "_self");
-});
 
